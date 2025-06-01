3d85cb72efb3bdb05b477a4e32e26bb2295a5184
Akash.V <120713761+Ak0000047@users.noreply.github.com>
2025-06-01 17:21:29 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b3p^-vw(3_ly6ns3yfquq9o9gsu8o0r+z78^%jkwx!!ahi%4fh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 
